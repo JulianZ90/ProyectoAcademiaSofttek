54f676a185f987601e671bce67b58b3a97d23a0d
Academia <Academia@LSTKBA79653.softtek.com>
2016-05-26 16:08:47 -0300

    Maxi push

---
+++ Academia/src/main/java/ar/com/softtek/action/BajaPacienteAction.java
@@ -0,0 +1,20 @@
+package ar.com.softtek.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import ar.com.softtek.model.Paciente;
+
+public class BajaPacienteAction extends ActionSupport{
+//
+//	public String execute() {
+//		
+//		if (pacienteBo.bajaPaciente(new Paciente())) {
+//            return "success";
+//        }
+//        else
+//        	return "error";
+//        
+//        
+//	}
+	
+}
---
+++ Academia/src/main/java/ar/com/softtek/action/NuevoPacienteAction.java
@@ -0,0 +1,36 @@
+package ar.com.softtek.action;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import ar.com.softtek.model.Paciente;
+
+
+ 
+public class NuevoPacienteAction extends ActionSupport{
+
+	public String execute() {
+				
+	        
+//			if (pacienteBo(new Paciente())) {
+//	            return "success";
+//	        }
+//	        else
+//	        	return "error";
+	    
+		if(true)
+		{
+			
+			return "success";
+		}
+		else
+			return "error";
+		}
+    
+
+	
+}
