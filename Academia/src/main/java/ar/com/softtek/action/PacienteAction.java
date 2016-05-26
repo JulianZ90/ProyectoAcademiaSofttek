4fb28775ce71eb956241314a04d7d87ed0846ceb
Maximiliano Kalaydjian <maxi.gkd@gmail.com>
2016-05-26 20:29:22 -0300

    Update PacienteAction.java

--- Academia/src/main/java/ar/com/softtek/action/PacienteAction.java
+++ Academia/src/main/java/ar/com/softtek/action/PacienteAction.java
@@ -1,17 +1,13 @@
 package ar.com.softtek.action;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+
 
 import com.opensymphony.xwork2.ModelDriven;
 
-import ar.com.softtek.bo.CustomerBo;
-import ar.com.softtek.model.Customer;
- 
+
 public class PacienteAction implements ModelDriven{
 
-	Paciente paciente = new Paciente();
+
 	
 	public Object getModel() {
 		// TODO Auto-generated method stub
