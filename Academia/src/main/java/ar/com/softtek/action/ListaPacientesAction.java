72839b891643dd7f6c2ab0dce635761b94ce00b4
Maximiliano Kalaydjian <maxi.gkd@gmail.com>
2016-05-29 21:36:56 -0300

    Banda de codigo funcional
    Una banda de codigo nuevo

---
+++ Academia/src/main/java/ar/com/softtek/action/ListaPacientesAction.java
@@ -0,0 +1,20 @@
+package ar.com.softtek.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+
+public class ListaPacientesAction extends ActionSupport{
+	
+//public String execute() {
+//
+//	if (pacienteBo.listaPacientes.length !=0){
+//		return "success";
+//    }
+//	else{
+//		return "error";
+//	}
+//	
+//
+//}
+}
