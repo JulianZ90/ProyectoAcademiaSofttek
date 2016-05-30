fb01a3eea911c33958889ec2aa9a05e65921cae5
Rodrigo <rodillob9@gmail.com>
2016-05-30 15:44:00 -0300

    Add files via upload
    agregada la vista baja de turnos

--- Archivos front end tp final/JSs/funcionalerta.js
+++ Archivos front end tp final/JSs/funcionalerta.js
@@ -10,3 +10,16 @@ function funcioncancelar(){
 		return true;
 	}
 }
+
+function cancelarTurno(){
+	var respconfirm = confirm("¿Esta seguro que desea cancelar? se perderan todos los datos no  guardados");
+	if (respconfirm == true) {
+		window.location.href = "../Busqueda Paciente/ Turnos del Paciente.html" 
+		return true;
+	}
+}
+
+function bajaTurno(){
+	alert("El Turno ha sido dado de baja exitosamente")
+	window.location.href = "../Busqueda Paciente/Busqueda de Paciente.html"
+}
