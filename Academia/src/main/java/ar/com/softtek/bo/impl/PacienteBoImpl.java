93edd1ec7ecfd72566e63557d2b5b9e923e1d69a
PRPablo <prietopablo89@gmail.com>
2016-05-30 12:39:15 -0300

    Update PacienteBoImpl.java
    PacienteBoImpl : agregué mapeo de DTO a POJO

--- Academia/src/main/java/ar/com/softtek/bo/impl/PacienteBoImpl.java
+++ Academia/src/main/java/ar/com/softtek/bo/impl/PacienteBoImpl.java
@@ -1,4 +1,5 @@
 package ar.com.softtek.bo.impl;
+
 import java.util.List;
 
 import ar.com.softtek.bo.PacienteBo;
@@ -13,17 +14,34 @@ public class PacienteBoImpl implements PacienteBo {
 	public void setPacienteDAO(PacienteDAO paciente){
 		this.pacienteDAO=paciente;
 	}
+	
+	private Paciente mapeo(NuevoAfiliadoDto pacienteDto) {
+		
+		Paciente paciente = new Paciente();
+		int telefono = Integer.parseInt(pacienteDto.getTelefono());
+		paciente.setNombre(pacienteDto.getNombre());
+		paciente.setApellido(pacienteDto.getApellido());
+		paciente.setDni(pacienteDto.getDni());
+		paciente.setTipoDni(pacienteDto.getTipoDni());
+		paciente.setTelefono(telefono);
+		paciente.setMail(pacienteDto.getMail());
+		paciente.setFecNac(pacienteDto.getFecNac());
+		paciente.setSexo(pacienteDto.getSexo());
+		paciente.setEstadoCivil(pacienteDto.getEstadoCivil());
+		paciente.setDireccion(pacienteDto.getDireccion());
+		paciente.setFamiliaresACargo(pacienteDto.getCantHijos());
+		paciente.setPlanMedico(pacienteDto.getPlanMedico().getDescripcion());
+		
+		return paciente;
+	}
 
 	public void addPaciente(NuevoAfiliadoDto pacienteDto) {
-		this.pacienteDAO.addPaciente(pacienteDto);
-		
+				
+		this.pacienteDAO.addPaciente(this.mapeo(pacienteDto));
 	}
 
 	public List<Paciente> listPaciente() {
 		return pacienteDAO.findAll();
 	}
-	
-	
-	
 	
 }
