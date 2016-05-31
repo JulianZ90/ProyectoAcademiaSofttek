ef0ab52b6cf23c8b26f20b234272859b2120b404
Academia <Academia@LSTKBA79653.softtek.com>
2016-05-31 14:57:18 -0300

    update

--- proyecto-grupob/src/main/java/ar/com/softtek/action/BajaPacienteAction.java
+++ proyecto-grupob/src/main/java/ar/com/softtek/action/BajaPacienteAction.java
@@ -2,13 +2,12 @@ package ar.com.softtek.action;
 
 import ar.com.softtek.dto.NuevoAfiliadoDto;
 
-public class NuevoPacienteAction extends PacienteAction {
+public class BajaPacienteAction extends PacienteAction {
 
 	@Override
 	public void action(NuevoAfiliadoDto nuevoAfiliadoDto) {
 		
-		this.pacienteBo.addPaciente(nuevoAfiliadoDto);
+		this.pacienteBo.deletePaciente(nuevoAfiliadoDto);
 	}
-	
-	
+
 }
