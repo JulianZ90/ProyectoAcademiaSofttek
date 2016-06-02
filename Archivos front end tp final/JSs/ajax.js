11224a30bca69ba7af47e94b61ea69cb5cc5442e
Rodrigo <rodillob9@gmail.com>
2016-06-02 15:11:30 -0300

    Add files via upload
    ajax para la función de busqueda

---
+++ Archivos front end tp final/JSs/ajax.js
@@ -0,0 +1,17 @@
+
+$("#idboton").click(function(){
+
+$.ajax({
+		url: "vistaAfiliado.jsp"
+		success: function (resultado) {},
+		
+		/*error: function() { 
+        	showWait(false); 
+        	Alerta.notify("Error en el servidor al llamar: " + link); 
+        },*/
+        complete: function(){
+        	// MG SPRINT25 CHROME
+        	//preventDefault(event);
+        }
+	});
+});
