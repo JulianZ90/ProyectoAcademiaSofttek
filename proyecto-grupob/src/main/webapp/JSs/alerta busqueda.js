9fcecede2b192b3c1cf0fdd78760388d026896b5
Academia <Academia@LSTKBA79648.softtek.com>
2016-06-02 14:44:56 -0300

    Borro proyecto

--- proyecto-grupob/src/main/webapp/JSs/alerta busqueda.js
+++
@@ -1,5 +0,0 @@
-function mostrarAlerta() {
-	var answer = alert ("No se pueden  mostrar los turnos, por favor seleccione un paciente primero.")
-	if (answer)
-	window.location="http://www.continue.com";
-}
